f70568e9fbdaadd5241ca13afc1b0d0470515cc1
SyntheticAutomation <mclampet@msudenver.edu>
2019-01-20 16:55:00 -0700

    Changes db from sqlite3 to pgsql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_01_20_043630) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
